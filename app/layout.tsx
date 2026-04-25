5f291943806e8ff343e00f38e3c8041c30e44540
sivaprakasam sivaprakasam <s.sivaprakasam-external@epexspot.com>
2026-04-25 07:25:37 +0100

    Add SiteTracker analytics + feedback widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -42,6 +43,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script
+          src="http://31.97.56.148:3098/t.js"
+          data-site="quicktechai.app"
+          strategy="lazyOnload"
+        />
       </body>
     </html>
   );
